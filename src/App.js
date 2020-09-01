23cfea544477b210d07b11b99bd4b7ac1057dca0
Raphael Beer <raphael.beer@protonmail.com>
2020-09-01 15:02:01 +0200

    Refactor: directory import for caster

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
     // Link
 } from 'react-router-dom'
 
-import { Caster } from './components/caster/caster'
+import { Caster } from './components/caster'
 import './App.css'
 
 function App() {
